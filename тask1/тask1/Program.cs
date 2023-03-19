bd25a6ed231aaa8423f9811a32894f35f6cd463d
Kiril <kiril.strykalenko@ilcoud.com>
2023-03-19 17:50:13 +0200

    lab3

---
+++ тask1/тask1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CSVReader;
+
+namespace тask1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\lab3\тask1\тask1\test.csv";
+            CSVReadering cSVReadering = new CSVReadering();
+            cSVReadering.Reader(path);
+        }
+    }
+}
